7c3c348a5aa800a10c94bcbf351fe96589a77a12
Ilya Maximov <xc.navi@gmail.com>
2022-05-05 16:46:42 +0200

    added ThreadExecutor

---
+++ src/Test.java
@@ -0,0 +1,53 @@
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+
+public class Test {
+    private Object lock1 = new Object();
+    private Object lock2 = new Object();
+
+    private void doWork1() {
+        synchronized (lock1) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException exception) {
+                exception.printStackTrace();
+                synchronized (lock2) {
+                    System.out.println("Working object 1");
+                }
+            }
+        }
+
+
+    }
+
+    private void doWork2() {
+        synchronized (lock2) {
+            try {
+                Thread.sleep(5005);
+            } catch (InterruptedException exception) {
+                exception.printStackTrace();
+                synchronized (lock1) {
+                    System.out.println("Working object 2");
+                }
+            }
+        }
+        synchronized (lock2) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        ExecutorService service = newFixedThreadPool(2);
+        service.execute(() -> test.doWork1());
+        service.execute(() -> test.doWork2());
+
+
+        service.shutdown();
+
+
+    }
+}
